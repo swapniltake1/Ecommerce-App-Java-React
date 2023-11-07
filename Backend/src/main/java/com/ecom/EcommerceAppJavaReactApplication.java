e785f2979853dd36b7232c57952166c62199bfc9
swapnil take <swapniltake1@outlook.com>
2023-11-07 10:55:04 +0530

    Initial commit: Add necessary project files for development

---
+++ Backend/src/main/java/com/ecom/EcommerceAppJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppJavaReactApplication.class, args);
+	}
+
+}
