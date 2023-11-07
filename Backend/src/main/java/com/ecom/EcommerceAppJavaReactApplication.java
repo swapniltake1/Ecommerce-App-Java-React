7841c632d8819b4c79d0cc5df5a6af668ea67eae
onkarlondhe <onkarrameshlondhe@gmail.com>
2023-11-07 13:17:04 +0530

    Added loginform

--- Backend/src/main/java/com/ecom/EcommerceAppJavaReactApplication.java
+++ Backend/src/main/java/com/ecom/EcommerceAppJavaReactApplication.java
@@ -8,6 +8,7 @@ public class EcommerceAppJavaReactApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceAppJavaReactApplication.class, args);
+		System.out.println("Application Started...");
 	}
 
 }
