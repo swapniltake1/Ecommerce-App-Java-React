5b8170d895d480bc268391d61be8513891ffb159
onkarlondhe <onkarrameshlondhe@gmail.com>
2023-11-08 14:41:25 +0530

    add

--- Backend/src/main/java/com/ecom/controller/UserController.java
+++ Backend/src/main/java/com/ecom/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.ecom.entity.User;
 import com.ecom.service.UserService;
@@ -18,19 +19,24 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-	//http://localhost:8080/techhub/createuser
+
+	// http://localhost:8080/techhub/createuser
 	@PostMapping("/createuser")
-	public ResponseEntity<String> createUser(@RequestBody User user){
-		
+	public ResponseEntity<String> createUser(@RequestBody User user) {
+
 		User saveUser = userService.saveUser(user);
-		
-		if(saveUser != null) {
+
+		if (saveUser != null) {
 			return new ResponseEntity<String>("User created Successfully", HttpStatus.OK);
-		}
-		else {
+		} else {
 			return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-	}
+
+	
+
+	@PostMapping("/add")
+	public ResponseEntity<String> addUserLogin(@RequestParam String email, @RequestParam String password){
+		userService.validateLogin(email,password);
+		return new ResponseEntity<String>(userService.addUserLogin(login), HttpStatus.CREATED);
 }
+	}
